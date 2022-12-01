a546efb0318bd8906d2270c547993651050a6b25
guruboyella1997 <guruboyella@gmail.com>
2022-12-01 14:23:32 +0530

    [guru]-Employeewage

---
+++ Employeewage/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fullTime = 1;
+Random ranobj = new Random();
+int checkpresent = ranobj.Next(0, 2);
+Console.WriteLine("Random number=" + checkpresent);
+if (checkpresent == fullTime)
+{
+    Console.WriteLine("Employee is present");
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
+
