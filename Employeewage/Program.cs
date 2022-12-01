4eb9816cbec200f4a68782acec5e18f8299402e9
guruboyella1997 <guruboyella@gmail.com>
2022-12-01 15:22:25 +0530

    [guru]-Parttimeemployeewage

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int fullTime = 1,  Emp_Rate_Per_Hoyr = 20,empHrs = 0,empwage = 0; 
+int fullTime = 1,  partTime=2,Emp_Rate_Per_Hoyr = 20,empHrs = 0,empwage = 0; 
 Random ranobj = new Random();
-int checkpresent = ranobj.Next(0, 2);
+int checkpresent = ranobj.Next(0, 3);
 Console.WriteLine("Random number=" + checkpresent);
 if (checkpresent == fullTime)
 {
     empHrs = 8;
     Console.WriteLine("Employee is present");
+}
+else if (checkpresent == partTime)
+{
+    empHrs = 4;
+    Console.WriteLine("Employee is Present");
 }
 else
 {
