cad172769964d18e03eb422cb8f0ba9b47dc9244
guruboyella1997 <guruboyella@gmail.com>
2022-12-01 16:13:54 +0530

    [guru]-switchcaseemployeewage

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -3,20 +3,20 @@ int fullTime = 1,  partTime=2,Emp_Rate_Per_Hoyr = 20,empHrs = 0,empwage = 0;
 Random ranobj = new Random();
 int checkpresent = ranobj.Next(0, 3);
 Console.WriteLine("Random number=" + checkpresent);
-if (checkpresent == fullTime)
+switch (checkpresent)
 {
-    empHrs = 8;
-    Console.WriteLine("Employee is present");
-}
-else if (checkpresent == partTime)
-{
-    empHrs = 4;
-    Console.WriteLine("Employee is Present");
-}
-else
-{
-    empwage = 0;
-    Console.WriteLine("Employee is absent");
+    case 1:
+        empHrs = 8;
+        Console.WriteLine("Employee is present");
+        break;
+    case 2:
+        empHrs = 4;
+        Console.WriteLine("Employee is Present");
+        break;
+    default:
+        empHrs = 0;
+        Console.WriteLine("Employe is absent");
+        break;
 }
 empwage = empHrs * Emp_Rate_Per_Hoyr;
 Console.WriteLine("Employee wage:" + empwage);
